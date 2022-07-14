258cbfcbecb17f00d67fb46a51484ede54b9dca7
icowibu <84970134+icowibu@users.noreply.github.com>
2022-07-14 21:15:24 +0700

    Delete public directory

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger')
-const pop = document.querySelector('.popout')
-const a = 22;
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('hamburger-active');
-    pop.classList.toggle('display-none')
-})
