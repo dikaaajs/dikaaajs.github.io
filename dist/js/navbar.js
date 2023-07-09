b77e9af8cf292f6bcd768e3ec64ba5cffd03532e
andika <zoneandika@gmail.com>
2023-07-10 00:42:53 +0700

    update sikit

--- dist/js/navbar.js
+++ dist/js/navbar.js
@@ -1,29 +1,35 @@
 const nav = document.querySelectorAll(".nav-btn");
 const sections = document.querySelectorAll(".sections");
+console.log(nav);
 
 function setActiveNav() {
   nav.forEach((i) => i.classList.remove("active"));
 
   const currentY = window.scrollY;
-  const positionAbout = sections[1].offsetTop - 10;
-  const positionRepo = sections[2].offsetTop - 10;
-  const positionContact = sections[3].offsetTop - 10;
+  const positionAbout = sections[1].offsetTop - 80;
+  const positionSkills = sections[2].offsetTop - 80;
+  const positionRepo = sections[3].offsetTop - 80;
+  const positionContact = sections[4].offsetTop - 80;
 
   // Home
   if (currentY < positionAbout) {
     nav[0].classList.add("active");
 
     // About
-  } else if (currentY < positionRepo) {
+  } else if (currentY > positionAbout && currentY < positionSkills) {
     nav[1].classList.add("active");
 
-    // Repository
-  } else if (currentY < positionContact) {
+    // skills
+  } else if (currentY > positionSkills && currentY < positionRepo) {
     nav[2].classList.add("active");
 
-    // Contact
-  } else if (currentY > positionContact) {
+    // Repository
+  } else if (currentY > positionRepo && currentY < positionContact) {
     nav[3].classList.add("active");
+
+    // Contact
+  } else if (currentY > positionRepo) {
+    nav[4].classList.add("active");
   }
 
   setDiv();
